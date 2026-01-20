311b1226d8763729743bbd03fa14c2aec2fda0a9
Michaell Alavedra <37460957+gitchaell@users.noreply.github.com>
2026-01-20 12:05:28 +0000

    chore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
@@ -9,21 +8,3 @@ export default defineConfig({
 		plugins: [tailwindcss()]
 	}
 })
-=======
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
-
-// https://astro.build/config
-export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
->>>>>>> b166f34 (feat: enhance Dreamspell page with responsive UI, analytics, and rich details)
